1144dd63771b97b60de53ae623159f8101857fa3
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-12-11 22:05:40 +0900

    Feat | Display movie list

--- app/src/main/java/com/example/rxmovie/MainActivity.kt
+++ app/src/main/java/com/example/rxmovie/MainActivity.kt
@@ -1,23 +1,38 @@
 package com.example.rxmovie
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.rxmovie.adapter.MovieAdapter
 import com.example.rxmovie.data.MovieViewModel
 import com.example.rxmovie.data.MovieViewModelFactory
+import com.example.rxmovie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var movieViewModel: MovieViewModel
+    private lateinit var movieViewModel: MovieViewModel
+    private lateinit var adapter: MovieAdapter
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        initRecyclerView()
+        bindData()
+    }
+
+    private fun initRecyclerView() {
+        adapter = MovieAdapter()
+        binding.recyclerview.adapter = adapter
+    }
+
+    private fun bindData() {
         movieViewModel = ViewModelProvider(this, MovieViewModelFactory()).get(MovieViewModel::class.java)
-        movieViewModel.movieList.observe(this) {
-            Log.d("movies", it.toString())
+        movieViewModel.movieList.observe(this) { movies ->
+            adapter.submitList(movies)
         }
         movieViewModel.getMovieList()
     }
