63c0cd52bcf153333314cf8a72be8e209e9710e4
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-12-11 22:05:22 +0900

    Add | Generate RecyclerView adapter for movie list

---
+++ app/src/main/java/com/example/rxmovie/adapter/MovieAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.rxmovie.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rxmovie.data.MovieModel
+import com.example.rxmovie.databinding.ItemMovieBinding
+
+class MovieAdapter : ListAdapter<MovieModel, MovieAdapter.MovieViewHolder>(diffUtil) {
+
+    inner class MovieViewHolder(private val binding: ItemMovieBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(movieModel: MovieModel) {
+            binding.movieModel = movieModel
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        return MovieViewHolder(ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<MovieModel>() {
+            override fun areItemsTheSame(oldItem: MovieModel, newItem: MovieModel): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: MovieModel, newItem: MovieModel): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
